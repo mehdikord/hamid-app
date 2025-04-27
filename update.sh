aa1989c2555039840d2bfb689e6c3683f8b7d7fd
Mehdi Kord <mehdi.kord171@gmail.com>
2025-04-27 12:10:31 +0330

    upgrade

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR;
 pwd;
-git pull origin master
-nvm use 23
-pnpm install
-pnpm run build
-cp .htaccess.example dist/.htaccess
+git pull origin master;
+nvm use 23;
+pnpm install;
+pnpm run build;
+cp .htaccess.example dist/.htaccess;
