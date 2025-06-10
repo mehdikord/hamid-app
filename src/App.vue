3304747cd6e7229ca45f604619a1e8f5a0ba4c5c
Mehdi Kord <mehdi.kord171@gmail.com>
2025-06-10 18:45:51 +0330

    update

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,95 @@
-<script>
-import Template_Menu from "@/components/template/Template_Menu.vue";
-import Template_App_Bar from "@/components/template/Template_App_Bar.vue";
-import {Stores_Auth} from "@/stores/auth/auth.js";
-
-export default {
-  name: "App",
-  data(){
-    return {
-      user_check : Stores_Auth().AuthGetCheckAuth,
-      drawer : true
-    }
-  },
-  components:{
-    'template_menu' : Template_Menu,
-    'template_appbar' : Template_App_Bar
-  }
-}
-</script>
-
 <template>
   <v-app>
-    <v-layout>
     <v-locale-provider rtl>
-    <v-main>
-      <v-container style="max-width: 100%!important;" >
+      <v-app-bar v-if="user_check" elevation="0" class="bg-grey-darken-4" app>
+        <v-app-bar-nav-icon variant="text" @click.stop="rail = !rail"></v-app-bar-nav-icon>
+        <v-toolbar-title>
 
-        <v-navigation-drawer
-            v-model="drawer"
-            location="start"
-            :mobile="true"
-        >
-          <div>
+        </v-toolbar-title>
 
-          </div>
-        </v-navigation-drawer>
-        <template_appbar v-if="user_check" ></template_appbar>
-        <router-view />
-        <template_menu v-if="user_check"></template_menu>
+        <template v-if="$vuetify.display.mdAndUp">
+          <v-btn icon="mdi-magnify" variant="text"></v-btn>
+          <v-btn icon="mdi-filter" variant="text"></v-btn>
+        </template>
 
-      </v-container>
-    </v-main>
+        <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer
+          v-if="user_check"
+          :rail="rail"
+          rail-width="65"
+          app
+          permanent
+          @click="rail = false"
+
+      >
+        <v-list nav>
+          <v-list-item
+          >
+            <template v-slot:prepend>
+              <v-avatar >
+                <img src="/src/assets/images/icons/profile.svg" width="100%" alt="">
+              </v-avatar>
+            </template>
+            <template v-slot:title>
+              <strong class="font-16">{{ user.name }}</strong>
+            </template>
+            <template v-slot:subtitle>
+              <div class="font-13 mt-1">{{ user.phone }}</div>
+            </template>
+          </v-list-item>
+          <v-divider class="mb-2"/>
+          <v-list-item
+              :to="{name : 'customers'}"
+          >
+            <template v-slot:prepend>
+              <v-icon icon="mdi mdi-account-group" size="30" color="deep-orange-darken-2">
+              </v-icon>
+            </template>
+            <template v-slot:title>
+              <div class="mt-1">
+                <strong class="font-14">شماره ها</strong>
+              </div>
+            </template>
+          </v-list-item>
+
+
+        </v-list>
+      </v-navigation-drawer>
+      <v-main>
+        <div class="v-container">
+          <router-view />
+
+        </div>
+      </v-main>
     </v-locale-provider>
-    </v-layout>
   </v-app>
 </template>
 
+<script>
+import Template_Menu from "@/components/template/Template_Menu.vue";
+import Template_App_Bar from "@/components/template/Template_App_Bar.vue";
+import { Stores_Auth } from "@/stores/auth/auth.js";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      user_check: Stores_Auth().AuthGetCheckAuth,
+      user: Stores_Auth().AuthGetUser,
+      rail: true,
+    };
+  },
+  components: {
+
+  },
+};
+</script>
+
 <style scoped>
-
-
-
+.main-content {
+  overflow-y: auto;
+  height: calc(100vh - 64px); /* بسته به ارتفاع app-bar */
+}
 </style>
