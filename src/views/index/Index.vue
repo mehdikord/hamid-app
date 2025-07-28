5a315ba46462fe42c6266d6e8c74c074e8a60828
Mehdi Kord <mehdi.kord171@gmail.com>
2025-07-28 16:01:04 +0330

    updated

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,8 +1,18 @@
-<script setup>
+<script >
+import Index_Links from "@/views/index/components/Index_Links.vue";
+
+export default {
+  name : "Index",
+  components : {
+    'links' : Index_Links,
+  }
+
+}
 
 </script>
 
 <template>
+  <links></links>
 
 </template>
 
---
+++ src/views/index/components/Index_Links.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  name: "Index_Links",
+
+}
+</script>
+
+<template>
+  <v-row>
+    <v-col cols="12" lg="6" xl="6" class="mt-1">
+      <router-link :to="{name : 'customers_consultant'}">
+        <v-row class="box">
+        <v-col cols="5" lg="4">
+          <img src="@/assets/images/icons/counseling.svg" class="image" alt="">
+        </v-col>
+        <v-col cols="7" lg="8">
+          <div class="mt-3">
+            <strong class="text-black">مشاوره</strong>
+          </div>
+          <div class="mt-2 text-grey-darken-2">
+            لیست شماره های در حال مشاوره
+          </div>
+        </v-col>
+      </v-row>
+      </router-link>
+    </v-col>
+    <v-col cols="12" lg="6" xl="6" class="mt-1">
+      <router-link :to="{name : 'customers_seller'}">
+        <v-row class="box">
+          <v-col cols="5" lg="4">
+            <img src="@/assets/images/icons/counseling.svg" class="image" alt="">
+          </v-col>
+          <v-col cols="7" lg="8">
+            <div class="mt-3">
+              <strong class="text-black">فروش</strong>
+            </div>
+            <div class="mt-2 text-grey-darken-2">
+              لیست شماره های در حال فروش
+            </div>
+          </v-col>
+        </v-row>
+      </router-link>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+.box{
+  padding: 2px 2px;
+  border: solid 1px rgba(0,0,0,0.08);
+  background-color:rgba(0,0,0,0.03);
+  border-radius: 10px;
+}
+.image{
+  width: 230px;
+}
+@media screen and (max-width: 600px) {
+
+  .image{
+    width: 160px;
+  }
+}
+
+
+</style>
